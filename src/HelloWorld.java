f0c2004385616ded53d1f4448224c7372b6569af
KennethStreet <kennethstreet@outlook.com>
2020-05-03 14:52:10 -0600

    src/HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World!");
+
+        int num = 5;
+        int sec = 5;
+        System.out.println("My number is: " + num + sec);
     }
 }
