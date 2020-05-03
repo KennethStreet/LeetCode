748084b542b27d4d6160861b4742548d7b5236e9
KennethStreet <kennethstreet@outlook.com>
2020-05-03 14:51:49 -0600

    Problem #27 - Remove Element

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -2,31 +2,23 @@
 public class RemoveElement {
 	public static void main(String args[]) {
 		
-		int[] elements = new int[] {3,2,2,3};
+//		int[] elements = new int[] {3,2,2,3};
+		int[] elements = new int[] {0,1,2,2,3,0,4,2};
 		
-		int len = elements.length;
-		
-		for(int i = 0; i < len; i++) {
-			System.out.println(elements[i]);
-		}
-		
-		int element = 3;
+		int element = 2;
 		
 		removeElement(elements, element);
 	}
 	
     public static int removeElement(int[] nums, int val) {
-    	
-    	for(int i = 0; i <= nums.length;) {
-    		if(nums[i] == val) {
-    			nums[i] = nums[i + 1];
-    			for(int j = 0; j < nums.length; j++) {
-    				System.out.println(nums[j]);
-    			}
-    		}
-    		i++;
-    	}
-        
-    	return nums.length;
+
+		int i = 0;
+		for (int j = 0; j < nums.length; j++) {
+			if (nums[j] != val) {
+				nums[i] = nums[j];
+				i++;
+			}
+		}
+		return i;
     }
 }
