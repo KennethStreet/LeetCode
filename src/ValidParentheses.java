d120a766b32286461b61ff4961050447336a1057
KennethStreet <kennethstreet@outlook.com>
2020-05-10 20:23:57 -0600

    Problem #20 - Valid Parentheses

---
+++ src/ValidParentheses.java
@@ -0,0 +1,16 @@
+public class ValidParentheses {
+
+    public static void main(String args[]) {
+        String check = "()";
+
+        isValid(check);
+    }
+
+    public static boolean isValid(String s) {
+        boolean parentheses = false;
+
+        
+
+        return parentheses;
+    }
+}
